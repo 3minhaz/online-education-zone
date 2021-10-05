b8ead032b894e08de6a35d0545d53a0be9667d59
3minhaz <3minhaz@gmail.com>
2021-10-05 22:43:20 +0600

    animation added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,17 +5,19 @@ import HeadingImage from '../../images/freeLogo.jpeg'
 
 const Header = () => {
     return (
-        <div className="header-container row py-5 bg-black">
-            <h3 className="header-name text-success pb-3 fs-1">Online Education Zone</h3>
+        <div className="header-container row py-5 bg-black ">
+            <h3 className=" text-success pb-3 fs-1 header">Online Education Zone</h3>
             <div className=" col-md-3 header-image">
                 <img className="rounded" src={HeadingImage} alt="" />
             </div>
             <div className="col-md-9 ">
                 <nav >
-                    <Link className="text-decoration-none pe-4 fs-3 text-white" to='/home'>Home</Link>
-                    <Link className="text-decoration-none pe-4 fs-3 text-white" to='/services'>Services</Link>
-                    <Link className="text-decoration-none pe-4 fs-3 text-white" to='/about'>About us</Link>
-                    <Link className="text-decoration-none  fs-3 text-white" to='/contact'>Contact</Link>
+                    <Link className="text-decoration-none me-4 fs-3 nav-item" to='/home'>Home</Link>
+                    <Link className="text-decoration-none me-4 fs-3 nav-item" to='/services'>Services</Link>
+                    <Link className="text-decoration-none  me-4 fs-3 
+                    nav-item" to='/about'>About us</Link>
+                    <Link className="text-decoration-none  fs-3 
+                    nav-item" to='/contact'>Contact</Link>
                 </nav>
             </div>
         </div>
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const NotFound = () => {
     return (
-        <div className="m-4 p-4">
+        <div style={{ padding: "130px 0px ", width: "50%" }} className="m-4   mx-auto">
+            <h1 className="text-primary">Oops!</h1>
             <h2 className="text-danger">404!!page not found</h2>
         </div>
     );
