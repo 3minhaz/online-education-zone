f68a1954098038772dc20c1caf57198f6b0bf724
3minhaz <3minhaz@gmail.com>
2021-10-05 19:52:48 +0600

    404 color change

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div className="m-4 p-4">
-            <h2>404!!page not found</h2>
+            <h2 className="text-danger">404!!page not found</h2>
         </div>
     );
 };
